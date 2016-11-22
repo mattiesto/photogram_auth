ff1cfb581dbd44891735a8cd41ccc8bb48c3e787
Matthias Mehrpuyan <mmehrpuyan@gmail.com>
2016-11-22 00:08:56 -0600

    15 Done

--- config/routes.rb
+++ config/routes.rb
@@ -53,11 +53,6 @@ Rails.application.routes.draw do
   get "/delete_photo/:id", :controller => "photos", :action => "destroy"
   #------------------------------
 
-  # Routes for the User resource:
-
-  # READ
-  get "/users", :controller => "users", :action => "index"
-  get "/users/:id", :controller => "users", :action => "show"
 
 
   devise_for :users
@@ -115,4 +110,11 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  # Routes for the User resource:
+
+  # READ
+  get "/users", :controller => "users", :action => "index"
+  get "/users/:id", :controller => "users", :action => "show"
+
 end
