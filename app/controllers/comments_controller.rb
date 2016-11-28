cfeff872ded307e7c0adee4f48c5a7b629fc2921
Matthias Mehrpuyan <mmehrpuyan@gmail.com>
2016-11-28 14:56:26 -0600

    Step 20 Almost Done

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -27,43 +27,43 @@ class CommentsController < ApplicationController
     save_status = @comment.save
 
     if save_status == true
-      redirect_to("/comments/#{@comment.id}", :notice => "Comment created successfully.")
-    else
-      render("comments/new.html.erb")
+        redirect_to("/comments/#{@comment.id}", :notice => "Comment created successfully.")
+      else
+        render("comments/new.html.erb")
+      end
     end
-  end
 
-  def edit
-    @comment = Comment.find(params[:id])
+    def edit
+      @comment = Comment.find(params[:id])
 
-    render("comments/edit.html.erb")
-  end
-
-  def update
-    @comment = Comment.find(params[:id])
-
-    @comment.photo_id = params[:photo_id]
-    @comment.body = params[:body]
-    @comment.user_id = params[:user_id]
-
-    save_status = @comment.save
-
-    if save_status == true
-      redirect_to("/comments/#{@comment.id}", :notice => "Comment updated successfully.")
-    else
       render("comments/edit.html.erb")
     end
-  end
 
-  def destroy
-    @comment = Comment.find(params[:id])
+    def update
+      @comment = Comment.find(params[:id])
 
-    @comment.destroy
+      @comment.photo_id = params[:photo_id]
+      @comment.body = params[:body]
+      @comment.user_id = params[:user_id]
 
-    if URI(request.referer).path == "/comments/#{@comment.id}"
-      redirect_to("/", :notice => "Comment deleted.")
-    else
-      redirect_to(:back, :notice => "Comment deleted.")
+      save_status = @comment.save
+
+      if save_status == true
+        redirect_to("/comments/#{@comment.id}", :notice => "Comment updated successfully.")
+      else
+        render("comments/edit.html.erb")
+      end
+    end
+
+    def destroy
+      @comment = Comment.find(params[:id])
+
+      @comment.destroy
+
+      if URI(request.referer).path == "/comments/#{@comment.id}"
+        redirect_to("/", :notice => "Comment deleted.")
+      else
+        redirect_to(:back, :notice => "Comment deleted.")
+      end
     end
   end
-end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @photos = @user.photos
+    @comments = @user.comments
 
     render("users/show.html.erb")
   end
